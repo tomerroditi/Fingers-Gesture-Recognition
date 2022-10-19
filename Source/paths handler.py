aa47a9ee10629751651fa851a79ebedf55419e13
tomer roditi <tomerroditi1@gmail.com>
2022-10-19 13:50:36 +0300

    started basic OOP

---
+++ Source/paths handler.py
@@ -0,0 +1,11 @@
+class Files_Handler():
+
+    def __init__(self):
+        pass
+
+    def create_path(self, subject, session, position, fixed_anotations = False):
+        pass
+
+    def load_file(self, path):
+        return signal, anotations
+
